347e6b5bfae0a42f6ebee68a0503df7346ac0153
yeisonLucio <yeisonlucio@gmail.com>
2020-01-12 21:53:58 -0500

    lista de anuncios completa a el 80 % login Registro

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <v-app>
 
+        <navegacion></navegacion>
+        <v-content>
+            <v-container>
+                 <router-view></router-view>
+
+            </v-container>
+            
+        </v-content>
+         <v-footer app>
+                <!-- -->
+        </v-footer>
+        
+    </v-app>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navegacion from './components/Navegacion.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: {Navegacion},
+    data() {
+        return {
+
+            
+        }
+    },
+    
+    
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import vueRouter from 'vue-router';
+import {routes} from './routes';
+import {store} from './store/store';
+
 
 Vue.config.productionTip = false
+Vue.use(vueRouter);
+
+Vue.filter('truncate', function (text, stop, clamp) {
+  return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+})
+
+const router = new vueRouter ({
+  history: true,
+  mode: 'history',
+  routes
+});
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
